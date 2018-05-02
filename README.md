37f9d4e181afd70bc60e9e9c9de16b00c2de2ea0
Unknown <kado_89_ssp_jun@yahoo.co.jp>
2018-05-02 22:29:46 +0900

    delete install

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 number_recognition
+
+python==3.6
+keras==1.2.2
+tensorflow==1.3.0
--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "create conda environment whose name is number"
-echo "installing..."
-
-conda create --file requirements.yaml -n number
-
-echo "finished"
-echo "activate number before call script"
