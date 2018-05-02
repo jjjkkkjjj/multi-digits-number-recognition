9cb96071da4b142092a16795cf2a4bae6231e63c
Unknown <kado_89_ssp_jun@yahoo.co.jp>
2018-05-02 21:56:50 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@
 echo "create conda environment whose name is number"
 echo "installing..."
 
-conda env create --file requrements.yaml -n number
+conda create --file requirements.yaml -n number
+
+echo "finished"
+echo "activate number before call script"
